43ca391bc29f6e50baedcdaa73137ff9014d83f6
Lenovo-User <Lenovo-User@DESKTOP-4ACQTBU>
2024-01-26 14:42:52 +0200

    Add project files.

---
+++ Anele Nontshokweni C# Assessment Part B/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Anele_Nontshokweni_C__Assessment_Part_B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lstWords = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(169, 111);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(90, 48);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChange.Location = new System.Drawing.Point(159, 201);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(109, 43);
+            this.btnChange.TabIndex = 1;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(162, 274);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(97, 43);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(174, 347);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 43);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(248, 52);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 26);
+            this.txtInput.TabIndex = 4;
+            // 
+            // lstWords
+            // 
+            this.lstWords.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstWords.FormattingEnabled = true;
+            this.lstWords.ItemHeight = 18;
+            this.lstWords.Location = new System.Drawing.Point(317, 124);
+            this.lstWords.Name = "lstWords";
+            this.lstWords.Size = new System.Drawing.Size(178, 220);
+            this.lstWords.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Anele_Nontshokweni_C__Assessment_Part_B.Properties.Resources.Img_7;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstWords);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnAdd);
+            this.ForeColor = System.Drawing.Color.Fuchsia;
+            this.Name = "Form1";
+            this.Text = "C# Assessment Part B";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ListBox lstWords;
+    }
+}
+
---
+++ Anele Nontshokweni C# Assessment Part B/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anele_Nontshokweni_C__Assessment_Part_B
+{
+    public partial class Form1 : Form
+    {
+        private List<string> wordsList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string word = txtInput.Text.Trim();
+            if (!string.IsNullOrEmpty(word))
+            {
+                wordsList.Add(word);
+                UpdateDisplay();
+            }
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            string selectedWord = GetSelectedWord();
+            if (selectedWord != null)
+            {
+                string newWord = txtInput.Text.Trim();
+                if (!string.IsNullOrEmpty(newWord))
+                {
+                    int index = wordsList.IndexOf(selectedWord);
+                    wordsList[index] = newWord;
+                    UpdateDisplay();
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            string selectedWord = GetSelectedWord();
+            if (selectedWord != null)
+            {
+                wordsList.Remove(selectedWord);
+                UpdateDisplay();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            wordsList.Clear();
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            lstWords.Items.Clear();
+            lstWords.Items.AddRange(wordsList.ToArray());
+            txtInput.Clear();
+        }
+
+        private string GetSelectedWord()
+        {
+            if (lstWords.SelectedIndex != -1)
+            {
+                return lstWords.SelectedItem.ToString();
+            }
+            return null;
+        }
+    }
+}
